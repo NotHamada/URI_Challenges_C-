a6c8722788fe450798d1e7a09a7f1a127026a8a3
Matheus Hamada <matheushamada@live.com>
2021-08-28 10:06:18 -0300

    URI

---
+++ Challenges/1001.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int a, b;
+    
+    cout << "";
+    cin >> a;
+    
+    cout << "";
+    cin >> b;
+    
+    int x = a + b;
+    cout << "X = " << x << endl;
+    
+    return 0;
+}
